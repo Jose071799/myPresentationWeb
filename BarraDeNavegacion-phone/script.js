45bb28f9e6ca437fc675054414d5d3df4768f162
Jose Carlos <josecsanchezfer@outlook.es>
2023-03-19 13:37:48 +0100

    funcionalidad a los botones menu

--- BarraDeNavegacion-phone/script.js
+++ BarraDeNavegacion-phone/script.js
@@ -7,3 +7,35 @@ function activeLink(){
 
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
+
+$('#contenido').load('home.html');
+
+$(document).ready(function() {
+    $('#home').click(function() {
+        $('#contenido').load('home.html');
+    });
+});
+
+$(document).ready(function() {
+    $('#about').click(function() {
+        $('#contenido').load('about.html');
+    });
+});
+
+$(document).ready(function() {
+    $('#service').click(function() {
+        $('#contenido').load('service.html');
+    });
+});
+
+$(document).ready(function() {
+    $('#contact').click(function() {
+        $('#contenido').load('contact.html');
+    });
+});
+
+$(document).ready(function() {
+    $('#login').click(function() {
+        $('#contenido').load('login.html');
+    });
+});
